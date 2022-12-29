2a3ef26b248fef1f5eb89ad07c67938da6eef2ea
hamzakhouzima <khouzimahamza20@gmail.com>
2022-12-29 19:53:53 +0100

    not working why , i don't know

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ var answers = Array.from(document.querySelectorAll(".choice-container"))
 var right,wrong
 var valid=[]
 var notValid=[]
+var aliasSerie=[]
+randomise(4)  
 
-
+let question_random=[];
 var x;
 let questions = [
     {
@@ -50,36 +52,29 @@ let questions = [
       choice4 : "food",
       answer : 1,
     },
+    
   ]
+
+
+console.log('rnaddd')
+console.log(question_random)
 
 
   var q=0;
   var score=0
 
-  getQuestion(q)
+//   getQuestion(q)
 function getQuestion(x){
-   
-    choices.forEach(element=>{
-
-        questionText.innerHTML=questions[x].question
-        questions.forEach((answer,index)=>{ 
-        if(x==index){
-            var number = element.dataset['number']
-            element.innerHTML = answer['choice' + number]  
-        }
-    });
-
-});
-}
-
-// }
-// function getQuestionCorr(correct){
-   
+       questionText.innerHTML=questions[x].question
+       document.querySelector('.ch-1').innerHTML=questions[x].choice1
+       document.querySelector('.ch-2').innerHTML=questions[x].choice2
+       document.querySelector('.ch-3').innerHTML=questions[x].choice3
+       document.querySelector('.ch-4').innerHTML=questions[x].choice4
 //     choices.forEach(element=>{
 
-//         questionText.innerHTML=questions[correct].question
+//         questionText.innerHTML=questions[x].question
 //         questions.forEach((answer,index)=>{ 
-//         if(correct==index){
+//         if(x==index){
 //             var number = element.dataset['number']
 //             element.innerHTML = answer['choice' + number]  
 //         }
@@ -87,94 +82,140 @@ function getQuestion(x){
 
 // });
 
-// }
+
+}
 
 
-getQuestion(q)
+
+
+    getQuestion(aliasSerie[q])
+
+    console.log('ftfft')
+    console.log(aliasSerie)
+
 
 function getAnswer(item){   
     var UserAnswer=item.getAttribute('data-number')
     // console.log(q)
-    let obj = {}
-    let RightAnswer = questions[q].answer
-    var q_id = questions[q].question
-    if(q <questions.length-1){
-    
-    increment()
-    getQuestion(q)
-    console.log(q)
-    }
-    else {
-        showResult(UserAnswer,RightAnswer,q) 
-    }
-    // console.log(questions[0].answer)
-    if(UserAnswer==RightAnswer){
-        // alert("riiiiiight biiitch")
-        score=score+10 
-        document.querySelector('#score').innerHTML = score
-
-        obj[q_id] = UserAnswer;
-        valid.push(obj) 
-        console.log(valid) 
+    // let obj = {}
+    let RightAnswer = questions[aliasSerie[q]].answer
+    // var cquestion = questions[q].question
+if(q <questions.length-1){
+            console.log(q)
+            
+            let countrrr=0;
+            console.log('totoot')
+            console.log(q)
+            question_random[q]=questions[aliasSerie[q]]
+            console.log(question_random)
+            increment()
+            getQuestion(q)      
+            if(UserAnswer==RightAnswer){
+                score=score+10 
+                document.querySelector('#score').innerHTML = score
         
-    }else{
-        // alert("not right ")
-        obj[q_id] = UserAnswer;
-        notValid.push(obj) 
-        console.log(notValid)
-    }
-    console.log("the right answer is :"+ RightAnswer)
-    console.log("ur answer is " + UserAnswer )
+                 }
+            else {
+                if(UserAnswer==RightAnswer){
+                    score=score+10 
+                    document.querySelector('#score').innerHTML = score          
+                }
+                }
+}else{
     
-   
- 
-    
-
+    question_random[q]=questions[aliasSerie[q]]
+    console.log(question_random)
+}
 }
 
-function showResult(right,user,qindex){
-    let ele1 =document.getElementsByClassName("choice-container")
-    let ele2 = document.getElementsByClassName("result")
-    document.getElementById("question").style.display='none'
-    
-   
 
-        document.getElementById("question_result").innerHTML=questions[qindex].question
+
+
+
+
+
+
+
+
+
+
+function showResult(check_userChoice,x){
+    let ele1 =document.getElementsByClassName("choice-container")
+    questionText.innerHTML=questions[x].question 
+    for(let count=0 ; count<questions.length;count++){
+        ele1[count].style.display="block"
+    }
+    document.querySelector('.ch-1').innerHTML=questions[x].choice1
+    document.querySelector('.ch-2').innerHTML=questions[x].choice2
+    document.querySelector('.ch-3').innerHTML=questions[x].choice3
+    document.querySelector('.ch-4').innerHTML=questions[x].choice4
+    let CorrectAnswer = questions[x].answer
+    // console.log(CorrectAnswer)
+
+
+    choices.forEach(element=>{
+    
+
         questions.forEach((answer,index)=>{ 
-        if(qindex==index){
-            var number = right
-            ele2.innerHTML = answer['choice' + number]  
-        }
+                if(x==index){ 
+                    var number = element.dataset['number']
+                    element.innerHTML = answer['choice' + number] 
+                    console.log(element)
+                    
+                }
+        
+        });
+
+   
     });
 
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    for(let count=0 ; count<questions.length;count++){
-        ele1[count].style.display="none"
-        
+ 
+if(check_userChoice==CorrectAnswer){
+        alert("this is the result")
     }
-    for(let j =0 ; j<2 ; j++){
-        ele2[j].style.display="block"
-    }
+    else{
+        alert('kassol')
+}
+
+
+}          
 
 
 
 
-
-
+function randomise(n){
+    var mainSerie = []
+    let qs= 0
+    var result
     
-
-            document.getElementById("ll").innerHTML= right
-            document.getElementById("ll2").innerHTML= user
+    for (var i = 0; i <= n ; i++) {
+        mainSerie.push(i)
+        //  console.log(i)
+     }
+    
+    var s=n
+    //  console.log(q)
+    while(aliasSerie.length<s){
+        let rand = Math.floor(Math.random() *  n)
+        qs=mainSerie[rand];
+      
+        n--
+        aliasSerie.push(qs)
+        // console.log(mainSerie.indexOf(q))
+        result = qs
+        // console.log(result)
+        mainSerie.splice(mainSerie.indexOf(qs),1)
+        // console.log("first "+q)
+    
+        // console.log("main  " + mainSerie)
+        //console.log(aliasSerie)
+        // return q
+    
+    
+    }
+    
+    }
+  
     
 
 
@@ -193,7 +234,7 @@ function showResult(right,user,qindex){
     
     // console.log(choices) 
 
-}
+
 
 
 
@@ -205,7 +246,7 @@ function showResult(right,user,qindex){
 function increment(){
   
 
-    q++
+    q=q+1
   
    
 
--- random.js
+++ random.js
@@ -1,9 +1,49 @@
- 
- 
+let questions = [
+    {
+      id : 1  ,
+      question : "what is the square root of 4",
+      choice1 : "1",
+      choice2 : "44",
+      choice3 : "2",
+      choice4 : "34",
+      answer : 3,
+    },
+    {
+      id : 2,
+      question : "ch7al sa3A",
+      choice1 : "16:44",
+      choice2 : "Sa3Ato llah",
+      choice3 : "05:43",
+      choice4 : "wa mada a3dadta laha ",
+      answer : 4,
+    },
+    {
+      id : 3,
+      question : "khouya fin jat lmslla",
+      choice1 : "dor m3A dora",
+      choice2 : "05 rue dar bou3za ",
+      choice3 : "sir nichan ",
+      choice4 : "hahia 7dak",
+      answer : 2,
+    },
+    {
+      id : 4,
+      question : "what is youcode ",
+      choice1 : "a school",
+      choice2 : "a philosophie",
+      choice3 : "a project",
+      choice4 : "food",
+      answer : 1,
+    },
+    
+  ]
+var newQuestion = []
+var aliasSerie=[]
+
 function randomise(n){
 var mainSerie = []
-var aliasSerie=[]
-var q= 0
+
+let q= 0
 var result
 
 for (var i = 0; i <= n ; i++) {
@@ -13,25 +53,34 @@ for (var i = 0; i <= n ; i++) {
 
 var s=n
 //  console.log(q)
-while(aliasSerie.length<=s){
-    q=mainSerie[Math.floor(Math.random() *  n)];
+while(aliasSerie.length<s){
+    let rand = Math.floor(Math.random() *  n)
+    q=mainSerie[rand];
+  
     n--
     aliasSerie.push(q)
     // console.log(mainSerie.indexOf(q))
     result = q
     // console.log(result)
     mainSerie.splice(mainSerie.indexOf(q),1)
-    console.log("first "+q)
+    // console.log("first "+q)
 
     // console.log("main  " + mainSerie)
+    console.log(aliasSerie)
+    // return q
 
-    
+
 }
 
-// console.log("random "+q)
-// console.log("alias " + aliasSerie)
-// 
-// console.log(q)
 }
- randomise(10)
+
+// let j
+
+// for(j=0;j<questions.length;j++){
+//    var p= newQuestion.fill(questions[j],randomise(questions.length))
+//    console.log(p) 
+
+// }
+
+ randomise(10)  
 //  randomise(5)
